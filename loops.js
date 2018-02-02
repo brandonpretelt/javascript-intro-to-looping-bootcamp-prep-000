bb673283579a74ed9ce4565edbf5e6a4a998e506
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 22:04:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (array[i] === 0) {
-      return "I am 1 strange loop.";
+      return array[i].push("I am 1 strange loop.");
     } else {
-      return `I am ${i} strange loops.`; 
+      return array[i].push(`I am ${i} strange loops.`); 
     }
   }
 }
